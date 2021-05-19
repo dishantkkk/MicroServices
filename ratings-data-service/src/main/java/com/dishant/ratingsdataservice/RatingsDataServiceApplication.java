a3cbb0cc3b72c37bf08c4ea5aab6a783b629d331
Dishant Kumar Kushwaha <36569773+dishantkkk@users.noreply.github.com>
2021-05-19 13:02:05 +0530

    Add files via upload

---
+++ ratings-data-service/src/main/java/com/dishant/ratingsdataservice/RatingsDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dishant.ratingsdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingsDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingsDataServiceApplication.class, args);
+	}
+
+}
